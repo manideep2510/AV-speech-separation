aedd6fc930662a9776f2865ebee7e3415577f31c
Durga Prasad <durgamaram98@gmail.com>
2019-09-12 18:23:19 +0530

    log_loss

--- losses.py
+++ losses.py
@@ -4,3 +4,8 @@ def l2_loss(spect1, spect2):
     loss = tf.sqrt(tf.nn.l2_loss(spect1[:,:,:,0] - spect2[:,:,:,0]))
     #loss = tf.sqrt(tf.nn.l2_loss(spect1 - spect2))
     return loss
+
+def cross_entropy_loss(target_mask, ground_truth_mask):
+
+	tbm_cross_entropy = (tf.nn.sigmoid_cross_entropy_with_logits(labels=target_mask, logits=ground_truth_mask)) 
+        return tf.reduce_sum(tbm_cross_entropy, name='binary_mask_loss')
