f544e6f22b269d950b4ba26c0b5d8858876f51e5
Durga Prasad <durgamaram98@gmail.com>
2019-10-21 16:32:28 +0530

    Update tasnet_lipnet.py

--- models/tasnet_lipnet.py
+++ models/tasnet_lipnet.py
@@ -17,7 +17,8 @@ from .lipnet import LipNet
 def custom_tanh(x,K=1,C=2):
     
     Cx=K*tf.math.divide(1-tf.math.exp(-1*C*x),1+tf.math.exp(-1*C*x))
-    Cy=tf.keras.backend.switch(Cx>0.9999999,tf.constant(0.9999999, dtype=tf.float32),Cx)
+    #Cy=tf.keras.backend.switch(Cx>0.9999999,tf.constant(0.9999999),Cx)
+    CY = 0.9999999*tf.dtypes.cast((x>0.9999999), dtype=tf.float32)+x*tf.dtypes.cast((x<=0.9999999), dtype=tf.float32)
     
     return Cy
 
